594dd5a13f73d0b3d12e994e3b292f6de9de104b
Sindy345 <sindy.sally.maria13579@gmail.com>
2024-02-16 10:57:20 -0500

    Test: ajouter méthode genererProgressionAccords, changement dans class Accord

--- src/ca/qc/bdeb/sim204/smartsounds/Test.py
+++ src/ca/qc/bdeb/sim204/smartsounds/Test.py
@@ -1,48 +1,87 @@
 import mingus.core.value as valeur
 import mingus.core.chords as chords
 import random
-class Accord:
+import mingus.core.scales as gamme
+import mingus.core.keys as keys
+
+#changement de tonalité plus tard
+class Note:
     nom = ""
-    pulsation = (4, 4) #meter 4/4, 3/4...
     durationStr = valeur.whole
     durationChiffre = 4
 
-    def __init__(self, nom, pusation, durationStr, durationChiffre):
+    def __init__(self, nom, duration_str, duration_chiffre):
         self.nom = nom
-        self.pulsation = pusation
-        self.durationStr = durationStr
-        self.durationChiffre = durationChiffre
+        self.durationStr = duration_str
+        self.durationChiffre = duration_chiffre
 
-    def __repr__(self): #for changing
+    def __repr__(self):  # pour changer
         return "<{0}>".format(self.nom)
 
-class AccordDefinition:
-    tonalite = "C"
-    def __init__(self, tonalite):
-        self.tonalite = tonalite
 
-    tonique = chords.tonic(tonalite)
-    progression_accord_debut = [tonique, chords.first_inversion(tonique), chords.second_inversion(tonique)]
+class Accord:
+    tonalite_majeure = "C"
+    tonalite_mineure = "c"
 
-    print(progression_accord_debut)
+    tonalite_majeure_list = keys.major_keys
+    # ['Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#']
+    tonalite_mineure_list = keys.minor_keys
+    # ['ab', 'eb', 'bb', 'f', 'c', 'g', 'd', 'a', 'e', 'b', 'f#', 'c#', 'g#', 'd#', 'a#']
+
+    # majeur
+    tonique = chords.I(tonalite_majeure)
+    progression_accord_debut_maj = [tonique, chords.first_inversion(tonique), chords.second_inversion(tonique)]
+    progression_accord_milieu_maj = []
+    for i in chords.triads(tonalite_majeure):
+        progression_accord_milieu_maj.append(i)
+    progression_accord_fin_maj = [chords.V7(tonalite_majeure), tonique]
+
+    # mineur
+    tonique = chords.I(tonalite_mineure)
+    print("tonique, mineure", tonique)
+    progression_accord_debut_min = [tonique, chords.first_inversion(tonique), chords.first_inversion(tonique),
+                                    chords.second_inversion(tonique)]
+    progression_accord_milieu_min = []
+    for i in chords.triads(tonalite_mineure):
+        progression_accord_milieu_min.append(i)
+    progression_accord_fin_min = [chords.V7(tonalite_mineure), tonique]
+
+    progression_accord_fin_min = [chords.V7(tonalite_mineure), tonique]
+    print(progression_accord_debut_min)
+
+
 class ProgressionAccords:
     progressionAccords = []
-    nombreMesure = 8 #possible de changer
-    def __init__(self, nombreMesure):
+    nombreMesure = 8  # possible de changer
+    tonalite = "C"
+    ambiance = "majeur"
+
+    def __init__(self, nombreMesure, tonalite, ambiance):
 
         self.nombreMesure = nombreMesure
+        self.tonalite = tonalite
+        self.ambiance = ambiance
 
-    def genererAccords(self):
+    def genererProgressionAccords(self):
         numeroMesure = 1
 
-      #  while (numeroMesure < self.nombreMesure - 2): #2 dernière mesures -> cadance
-           # if numeroMesure == 1:
-               # self.progressionAccords.append(random.sample())
+
+        if self.ambiance == "majeur":
+            self.progressionAccords.append(random.choice(Accord.progression_accord_debut_maj))
+            for i in range(5):
+                self.progressionAccords.append(random.choice(Accord.progression_accord_milieu_maj))
+            for i in Accord.progression_accord_fin_min:
+                self.progressionAccords.append(i)
+
+        elif self.ambiance == "mineur":
+            self.progressionAccords.append(random.choice(Accord.progression_accord_debut_min))
+            for i in range(5):
+                 self.progressionAccords.append(random.choice(Accord.progression_accord_milieu_min))
+            for i in Accord.progression_accord_fin_min:
+                self.progressionAccords.append(i)
+        print("progression des accords", self.progressionAccords)
+        print(len(self.progressionAccords))
 
 
-accords_C_majeur_tonique = AccordDefinition("C")
-print(accords_C_majeur_tonique)
-
-
-
-
+test = ProgressionAccords(8, "C", "mineur")
+print(test.genererProgressionAccords())
