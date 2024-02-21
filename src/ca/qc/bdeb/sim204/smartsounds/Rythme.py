9a4b0f34a2ea8d26594a0dd9695ad479fc078753
Sindy345 <sindy.sally.maria13579@gmail.com>
2024-02-21 12:03:30 -0500

    class Rythme: attribut "progression" changer de propriété à class ProgressionAccords, class test: pour tester

---
+++ src/ca/qc/bdeb/sim204/smartsounds/Rythme.py
@@ -0,0 +1,29 @@
+import mingus.core.value as valeur
+import random
+
+
+class Rythme:
+    choix_notes = [4 / valeur.whole, 4 / valeur.half, 4 / valeur.quarter, 4 / valeur.eighth]
+    # [4.0, 2.0, 1.0, 0.5]
+    choix_mesure = [(3, 4), (4, 4), (5, 4)]
+    list_duree_note = []
+    mesure = ()
+
+    def __init__(self, mesure_utilisateur):
+        if mesure_utilisateur is None:
+            self.mesure = random.choice(self.choix_mesure)
+        else:
+            self.mesure = mesure_utilisateur
+
+    # generer durée des notes par mesure
+    def genererRythme(self):
+        i = 0
+        while i < self.mesure[0]:
+            choix = random.choice(self.choix_notes)
+            i += choix
+            if i > 4:
+                i -= choix
+            else:
+                self.list_duree_note.append(choix)
+
+        print("Class: Rythme; list_duree_note: ",self.list_duree_note)
---
+++ src/ca/qc/bdeb/sim204/smartsounds/test.py
@@ -0,0 +1,6 @@
+import Melody
+import ProgressionAccords
+import Rythme
+
+melody = Melody.Melody((3, 4), "D")
+melody.generer_melodie()
