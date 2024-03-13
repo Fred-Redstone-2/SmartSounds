1eb087e5027ae6296026d35be6e7862b4dfda286
PablitoGM <pa.gmontero@gmail.com>
2024-03-13 10:58:18 -0400

    Modification du UI : - Ajout du bouton Générer, Exporter et Jouer - Ajout du placeholder de la partition - Ajout de condition des tonalité

--- src/ca/qc/bdeb/sim204/smartsounds/Interface/UI.py
+++ src/ca/qc/bdeb/sim204/smartsounds/Interface/UI.py
@@ -1,6 +1,10 @@
+import tkinter
 import tkinter as tk
-from tkinter import ttk, GROOVE, TOP, X
-from tkinter.messagebox import showinfo
+from tkinter import *
+from tkinter import ttk
+
+
+
 
 root = tk.Tk()
 root.title('Séquence Sonore')
@@ -12,29 +16,77 @@ root.geometry("%dx%d" % (width, height))
 canvas = tk.Canvas(root, width=(width / 2), height=height - 300, bg='white')
 canvas.place(x=50, y=50)
 
+
+
 # Tonalité Label
 textTonalite = tk.Label(root, text="Tonalité :", font=("Eras Demi ITC", 32), bg="white")
 textTonalite.place(x=150, y=150)
 
 # Tonalité Combobox
 n = tk.StringVar()
-tonalite = ttk.Combobox(root, width=15, textvariable=n, font=("Eras Demi ITC", 32))
-tonalite['values'] = ('Do Maj',
-                      'Ré Maj',
-                      'Mi Maj',
-                      'Fa Maj',
-                      'Sol Maj',
-                      'La Maj',
-                      'Si Maj',
-                      'Do♯ Maj',
-                      'Mi♭ Maj',
-                      'Fa♯ Maj',
-                      'La♭ Maj',
-                      'Si♭ Maj')
+n2 = tk.StringVar()
+tonalite = tkinter.ttk.Combobox(
+    root,
+    width=6,
+    textvariable=n,
+    font=("Eras Demi ITC", 32)
+
+)
+
+tonalite['values'] = ('Do',
+                      'Ré',
+                      'Mi',
+                      'Fa',
+                      'Sol',
+                      'La',
+                      'Si',
+                      'Do♯',
+                      'Ré♭',
+                      'Mi♭',
+                      'Fa♯',
+                      'Sol♭',
+                      'La♭',
+                      'Si♭'
+                      )
 tonalite['state'] = 'readonly'
 tonalite.place(x=450, y=150)
-
 tonalite.current(0)
+
+majeurOuMineur = tkinter.ttk.Combobox(
+    root,
+    width=6,
+    textvariable=n2,
+    font=("Eras Demi ITC", 32)
+)
+majeurOuMineur['values'] =('Maj',
+                           'Min')
+majeurOuMineur['state'] = 'readonly'
+majeurOuMineur.place(x=650, y=150)
+majeurOuMineur.current(0)
+
+def tonalite_change(event):
+    if "Ré♭" in n.get() :
+        majeurOuMineur['values'] = ('Maj')
+        majeurOuMineur.current(0)
+
+    elif "Fa♯" in n.get() :
+        majeurOuMineur['values'] = ('Min')
+        majeurOuMineur.current(0)
+
+    elif "Do♯" in n.get() :
+        majeurOuMineur['values'] = ('Min')
+        majeurOuMineur.current(0)
+
+    elif "Sol♭" in n.get() :
+        majeurOuMineur['values'] = ('Maj')
+        majeurOuMineur.current(0)
+
+    else :
+        majeurOuMineur['values'] = ("Maj",
+                                    "Min")
+
+tonalite.bind('<<ComboboxSelected>>', tonalite_change)
+
 
 # Tempo Label
 textTempo = tk.Label(root, text="Tempo :", font=("Eras Demi ITC", 32), bg="white")
@@ -84,6 +136,8 @@ temposlider.set(30)
 
 value_label.place(x=540, y=325)
 bpm_label.place(x=630, y=330)
+
+
 temposlider.place(x=425, y=375)
 
 # Durée Label
@@ -96,7 +150,53 @@ duree = ttk.Combobox(root, width=15, textvariable=g, font=("Eras Demi ITC", 32))
 duree['values'] = ('4 mesures', '8 mesures', '16 mesures', '32 mesures', '64 mesures')
 duree['state'] = 'readonly'
 duree.place(x=450, y=550)
-
 duree.current(0)
+
+# Bouton GENERER
+
+imgGen = PhotoImage(file = r"C:\Users\2236975\Downloads\SmartSounds\src\ca\qc\bdeb\sim204\smartsounds\Interface\U+266B_a.svg (1).png")
+
+btnGenerer = tk.Button(
+    root,
+    text="Générer",
+    width = 500,
+    height = 150,
+    font=("Informal Roman", 60, 'bold'),
+    foreground = 'black',
+    image = imgGen,
+    compound=tk.LEFT
+)
+btnGenerer.place(x = 150, y = 775)
+
+# Button Exporter
+
+imgExpo = PhotoImage(file= r"C:\Users\2236975\Downloads\SmartSounds\src\ca\qc\bdeb\sim204\smartsounds\Interface\expo-ezgif.com-webp-to-png-converter.png")
+
+btnExporter = tk.Button(
+    root,
+    image= imgExpo,
+    width = 150,
+    height=150
+)
+btnExporter.place(x = 700, y = 775)
+
+# Affichage de la partition
+partition = PhotoImage(file = r"C:\Users\2236975\Downloads\SmartSounds\src\ca\qc\bdeb\sim204\smartsounds\Interface\O_Canada_sheet_music.png")
+imgPart = ttk.Label(
+    root,
+    image=partition,
+    padding=5
+)
+imgPart.place(x = 1200, y = 50)
+
+# Bouton Jouer
+jouer = PhotoImage(file=r"C:\Users\2236975\Downloads\SmartSounds\src\ca\qc\bdeb\sim204\smartsounds\Interface\play-button-6 (1).png")
+btnJouer = Button(
+    root,
+    image=jouer,
+    height = 100,
+    width= 100
+)
+btnJouer.place(x = 1400, y = 810)
 
 root.mainloop()
